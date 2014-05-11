c879927a82deeec30deff773ec865e9d79d6da82
Simon Scarfe <simon.scarfe@gmail.com>
2014-05-11 23:54:34 +0100

    updated parentheses tests

--- python/test_parentheses.py
+++ python/test_parentheses.py
@@ -12,5 +12,33 @@ class ParenthesesTestCase(unittest.TestCase):
         self.assertTrue(has_balanced_parentheses_one(''))
         self.assertTrue(has_balanced_parentheses_two(''))
 
+    def test_single_set_braces(self):
+        self.assertTrue(has_balanced_parentheses_one('()'))
+        self.assertTrue(has_balanced_parentheses_two('()'))
+
+    def test_invalid_set_braces(self):
+        self.assertFalse(has_balanced_parentheses_one('('))
+        self.assertFalse(has_balanced_parentheses_two('('))
+
+    def test_square_braces(self):
+        self.assertTrue(has_balanced_parentheses_one('[]')
+        self.assertTrue(has_balanced_parentheses_two('[]')
+
+    def test_mixed_braces(self):
+        self.assertTrue(has_balanced_parentheses_one('(){}[]')
+        self.assertTrue(has_balanced_parentheses_two('(){}[]')
+
+    def test_nested_braces(self):
+        self.assertTrue(has_balanced_parentheses_one('({[]})')
+        self.assertTrue(has_balanced_parentheses_two('({[]})')
+
+    def test_some_lisp(self):
+        self.assertTrue(has_balanced_parentheses_one('(sin (* 2 pi 1/4))')
+        self.assertTrue(has_balanced_parentheses_two('(sin (* 2 pi 1/4))')
+
+    def test_bad_mixed(self):
+        self.assertFalse(has_balanced_parentheses_one('{(])[}')
+        self.assertFalse(has_balanced_parentheses_two('{(])[}')
+
 if __name__ == '__main__':
     unittest.main()

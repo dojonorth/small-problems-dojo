516c335a12e6e0e62a993d5454ee55744c577942
Simon Scarfe <simon.scarfe@gmail.com>
2014-05-23 13:35:36 +0100

    fixed unbalanced parentheses

--- python/test_parentheses.py
+++ python/test_parentheses.py
@@ -25,32 +25,32 @@ class ParenthesesTestCase(unittest.TestCase):
         self.assertFalse(has_balanced_parentheses_two(')'))
         
     def test_square_braces(self):
-        self.assertTrue(has_balanced_parentheses_one('[]')
-        self.assertTrue(has_balanced_parentheses_two('[]')
+        self.assertTrue(has_balanced_parentheses_one('[]'))
+        self.assertTrue(has_balanced_parentheses_one('[]'))
 
     def test_mixed_braces(self):
-        self.assertTrue(has_balanced_parentheses_one('(){}[]')
-        self.assertTrue(has_balanced_parentheses_two('(){}[]')
+        self.assertTrue(has_balanced_parentheses_one('(){}[]'))
+        self.assertTrue(has_balanced_parentheses_two('(){}[]'))
 
     def test_nested_braces(self):
-        self.assertTrue(has_balanced_parentheses_one('({[]})')
-        self.assertTrue(has_balanced_parentheses_two('({[]})')
+        self.assertTrue(has_balanced_parentheses_one('({[]})'))
+        self.assertTrue(has_balanced_parentheses_two('({[]})'))
 
     def test_some_lisp(self):
-        self.assertTrue(has_balanced_parentheses_one('(sin (* 2 pi 1/4))')
-        self.assertTrue(has_balanced_parentheses_two('(sin (* 2 pi 1/4))')
+        self.assertTrue(has_balanced_parentheses_one('(sin (* 2 pi 1/4))'))
+        self.assertTrue(has_balanced_parentheses_two('(sin (* 2 pi 1/4))'))
 
     def test_multiple_nested_groups(self):
-        self.assertTrue(has_balanced_parentheses_one('(+ (* 2 pi 1/4)(/ 8 (- 6 1)))')
-        self.assertTrue(has_balanced_parentheses_two('(+ (* 2 pi 1/4)(/ 8 (- 6 1)))')
+        self.assertTrue(has_balanced_parentheses_one('(+ (* 2 pi 1/4)(/ 8 (- 6 1)))'))
+        self.assertTrue(has_balanced_parentheses_two('(+ (* 2 pi 1/4)(/ 8 (- 6 1)))'))
 
     def test_bad_mixed(self):
-        self.assertFalse(has_balanced_parentheses_one('{(])[}')
-        self.assertFalse(has_balanced_parentheses_two('{(])[}')
+        self.assertFalse(has_balanced_parentheses_one('{(])[}'))
+        self.assertFalse(has_balanced_parentheses_two('{(])[}'))
         
     def test_broken_nested_groups(self):
-        self.assertFalse(has_balanced_parentheses_one('(()([}))')
-        self.assertFalse(has_balanced_parentheses_two('(()([}))')
+        self.assertFalse(has_balanced_parentheses_one('(()([}))'))
+        self.assertFalse(has_balanced_parentheses_two('(()([}))'))
 
 
 if __name__ == '__main__':
